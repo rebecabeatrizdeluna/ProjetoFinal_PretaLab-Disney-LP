6b2282a1ce32a96f0f3137248e4f5c45a0ae2b72
Rebeca Beatriz de Luna <rebecabeatrizdeluna@gmail.com>
2023-03-10 19:09:30 -0300

    Teste calculadora

--- js/calculadora.js
+++ js/calculadora.js
@@ -4,12 +4,12 @@ function somar() {
   // evite mexer no código acima!
 
 
-
+const soma = (numero1 + numero2);
 
 
   /* use as constantes numero 1 e numero 2 e realize a soma entre elas.
   Em seguida, ao invés de mostrar o numero 10, mostre o resultado da operação */
-  document.getElementById("resultadoSoma").innerHTML = 10;
+  document.getElementById("resultadoSoma").innerHTML = soma; 
 }
 
 function subtrair() {
@@ -18,12 +18,12 @@ function subtrair() {
   // evite mexer no código acima!
 
 
-
+const subtracao = (numero1 - numero2);
 
 
   /* use as constantes numero 1 e numero 2 e realize a soma entre elas.
    Em seguida, ao invés de mostrar o numero 9, mostre o resultado da operação */
-  document.getElementById("resultadoSub").innerHTML = 9;
+  document.getElementById("resultadoSub").innerHTML = subtracao;
 }
 
 function multiplicar() {
@@ -32,12 +32,12 @@ function multiplicar() {
   // evite mexer no código acima!
 
 
-
+const multiplicacao = (numero1 * numero2);
 
 
   /* use as constantes numero 1 e numero 2 e realize a soma entre elas.
    Em seguida, ao invés de mostrar o numero 5, mostre o resultado da operação */
-  document.getElementById("resultadoMult").innerHTML = 5;
+  document.getElementById("resultadoMult").innerHTML = multiplicacao;
 }
 
 function dividir() {
@@ -46,7 +46,7 @@ function dividir() {
   // evite mexer no código acima!
 
 
-
+const divisao = (numero1 / numero2)
 
   
   /* use as constantes numero 1 e numero 2 e realize a soma entre elas.
