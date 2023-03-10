7cdf319ed6f50cf3f98132c78751aaf865f440c1
Rebeca Beatriz de Luna <rebecabeatrizdeluna@gmail.com>
2023-03-10 19:14:33 -0300

    Teste final calculadora

--- js/calculadora.js
+++ js/calculadora.js
@@ -51,5 +51,5 @@ const divisao = (numero1 / numero2)
   
   /* use as constantes numero 1 e numero 2 e realize a soma entre elas.
    Em seguida, ao invés de mostrar o numero -89, mostre o resultado da operação */
-  document.getElementById("resultadoDiv").innerHTML = -89;
+  document.getElementById("resultadoDiv").innerHTML = divisao;
 }
